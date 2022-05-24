2ebbc9e6e961c5493c3be4322d9e653d0f69f9bc
codewithshamim <shamimislamonline@gmail.com>
2022-05-24 13:48:02 +0600

    reviews rating added

--- src/components/Home/Reviews/ReviewDetail.js
+++ src/components/Home/Reviews/ReviewDetail.js
@@ -1,12 +1,49 @@
 import React from "react";
 import defaultAvatar from "../../../images/avatar.png";
+import { FaStar } from "react-icons/fa";
 
 const ReviewDetail = ({ review }) => {
   const { name, avatar, rating, description } = review;
+  const ratingNumber = parseInt(rating);
+
   return (
     <div className=" shadow-xl rounded-lg p-4 w-full overflow-hidden">
       <h1>{description}</h1>
-      <p>Rating: {rating}/5</p>
+      <p className="text-orange-200 flex items-center">
+        <span className="text-blue-800 font-semibold pr-3">Rating: </span>
+
+        {ratingNumber === 1 && <FaStar />}
+        {ratingNumber === 2 && (
+          <>
+            <FaStar />
+            <FaStar />
+          </>
+        )}
+        {ratingNumber === 3 && (
+          <>
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </>
+        )}
+        {ratingNumber === 4 && (
+          <>
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </>
+        )}
+        {ratingNumber === 5 && (
+          <>
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </>
+        )}
+      </p>
       <div className="flex justify-evenly items-center pt-12">
         <img
           className="w-20 h-20 rounded-full"
