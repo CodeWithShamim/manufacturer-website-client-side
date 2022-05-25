1c4d1e529ecd57e5490f16cbcf30f1d6afac1f34
codewithshamim <shamimislamonline@gmail.com>
2022-05-25 14:11:14 +0600

    stripte setup added

---
+++ src/components/Dashboard/Payment/CheckoutForm .js
@@ -0,0 +1,31 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React from "react";
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (elements == null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe || !elements}>
+        Pay
+      </button>
+    </form>
+  );
+};
+
+export default CheckoutForm;
--- src/components/Dashboard/Payment/Payment.js
+++ src/components/Dashboard/Payment/Payment.js
@@ -1,7 +1,10 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import Loading from "../../Shared/Loading/Loading";
+import CheckoutForm from "./CheckoutForm ";
 
 const Payment = () => {
   const { id } = useParams();
@@ -14,8 +17,11 @@ const Payment = () => {
     return <Loading />;
   }
 
+  //   _________stripe info______
+  const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
+
   return (
-    <div className="p-8 m-3 md:m-10 bg-base-100 text-left rounded-lg font-serif flex items-center justify-center">
+    <div className=" flex flex-col md:flex-row items-center justify-center p-8 m-3 md:m-10 bg-base-100 text-left rounded-lg font-serif">
       {/* ---left side---  */}
       <div className="flex-1">
         <h1 className="text-primary text-2xl">Hello, {data?.name}</h1>
@@ -23,23 +29,30 @@ const Payment = () => {
           Please pay for
           <span className="text-cyan-400"> {data?.orderName}</span>
         </h1>
-        <div className="card w-96 glass">
+        <div className="card w-full glass">
           <figure>
-            <img src={data?.img} alt="item-img" />
+            <img className="w-72 h-72" src={data?.img} alt="item-img" />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title font-sans font-semibold text-xl text-green-400">
-              Total Pay:
-              <span className="font-sans font-semibold text-xl text-black">
-                ${data?.totalPrice}
-              </span>
-            </h2>
-          </div>
+          {/* <div className="card-body"></div> */}
         </div>
       </div>
 
       {/* ---right side--*/}
-      <div className="bg-secondary">ffff</div>
+      {/* __________stripe payment form________ */}
+      <div className="bg-base-200 p-24 rounded-lg shadow-xl flex-1 md:mx-6 mt-6 md:mt-24">
+        <h2 className="my-6 card-title font-sans font-semibold text-xl text-green-400">
+          Total Pay:
+          <span className="font-sans font-semibold text-xl text-black">
+            ${data?.totalPrice}
+          </span>
+        </h2>
+        {/* _______card form_______ */}
+        <div>
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </div>
     </div>
   );
 };
