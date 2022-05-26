6c9f68e444e01bc0004302b5dbd9502825f06df5
codewithshamim <shamimislamonline@gmail.com>
2022-05-26 19:00:36 +0600

    not found page added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Blogs = () => {
   return (
-    <div className="text-left p-4 md:p-12 font-serif">
+    <div className="text-left px-4 py-8 md:pt-12 md:px-24 font-serif">
       {/* ___question 1___  */}
       <div>
-        <h1 className="text-lg md:xl lg:text-2xl text-left text-gray-black font-semibold pb-2">
+        <h1 className="text-lg md:xl text-left text-gray-black font-semibold pb-2">
           How will you improve the performance of a React Application?
         </h1>
         <p>
@@ -17,7 +17,7 @@ const Blogs = () => {
 
       {/* ___question 2___  */}
       <div className="mt-10">
-        <h1 className="text-lg md:xl lg:text-2xl text-left text-gray-black font-semibold pb-2">
+        <h1 className="text-lg md:xl text-left text-gray-black font-semibold pb-2">
           What are the different ways to manage a state in a React application?
         </h1>
         <p>
@@ -29,7 +29,7 @@ const Blogs = () => {
 
       {/* ___question 3___  */}
       <div className="mt-10">
-        <h1 className="text-lg md:xl lg:text-2xl text-left text-gray-black font-semibold pb-2">
+        <h1 className="text-lg md:xl text-left text-gray-black font-semibold pb-2">
           How does prototypical inheritance work?
         </h1>
         <p>
@@ -41,7 +41,7 @@ const Blogs = () => {
 
       {/* ___question 4___  */}
       <div className="mt-10">
-        <h1 className="text-lg md:xl lg:text-2xl text-left text-gray-black font-semibold pb-2">
+        <h1 className="text-lg md:xl text-left text-gray-black font-semibold pb-2">
           Why you do not set the state directly in React. For example, if you
           have const [products, setProducts] = useState([]). Why you do not set
           products = [...] instead, you use the setProducts.
@@ -55,7 +55,21 @@ const Blogs = () => {
 
       {/* ___question 5___  */}
       <div className="mt-10">
-        <h1 className="text-lg md:xl lg:text-2xl text-left text-gray-black font-semibold pb-2">
+        <h1 className="text-lg md:xl text-left text-gray-black font-semibold pb-2">
+          You have an array of products. Each product has a name, price,
+          description, etc. How will you implement a search to find products by
+          name?
+        </h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste sequi
+          ipsa harum ut fugit, quos, maiores nostrum ullam libero voluptas enim
+          in iure rem sed nihil vero architecto magnam veritatis!
+        </p>
+      </div>
+
+      {/* ___question 5___  */}
+      <div className="mt-10">
+        <h1 className="text-lg md:xl text-left text-gray-black font-semibold pb-2">
           What is a unit test? Why should write unit tests?
         </h1>
         <p>
--- src/components/Shared/NotFound.js
+++ src/components/Shared/NotFound.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import notFound from "../../images/not-found.jpg";
 
 const NotFound = () => {
   return (
-    <div className="min-h-screen">
-      <h1 className="text-red-500 text-4xl p-16">Opps, Not Found</h1>
-      <h1 className="text-primary text-2xl uppercase font-semibold">
-        <Link to="/">Go To Home</Link>
-      </h1>
+    <div
+      className="min-h-screen"
+      style={{
+        backgroundImage: `url(${notFound})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="pt-32 md:pt-60">
+        <h1 className="text-base-100 text-4xl bg-black p-2 uppercase  font-semibold">
+          <Link to="/">Go To Home</Link>
+        </h1>
+      </div>
     </div>
   );
 };
