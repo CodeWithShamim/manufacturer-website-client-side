b5a0ca22874ddc003050f881df35644f62351710
codewithshamim <shamimislamonline@gmail.com>
2022-06-01 14:01:57 +0600

    dashboard updated

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import useAdmin from "../../hooks/useAdmin";
+import { FaUserEdit, FaHandHoldingUsd, FaRegComments } from "react-icons/fa";
 
 const Dashboard = () => {
   const [isAdmin] = useAdmin();
@@ -8,18 +9,33 @@ const Dashboard = () => {
   return (
     <div className="bg-cyan-700 flex flex-col md:flex-row justify-center items-start">
       {/* ___dropdown link here___  */}
-      <div className="bg-cyan-900 text-base-100 font-sans md:font-serif justify-center items-center md:justify-start w-full md:w-60 list-none flex md:flex-col p-6 md:h-screen shadow-2xl">
-        <ul className="grid gap-3 grid-flow-col md:grid-flow-row text-sm text-left">
-          <li>
-            <NavLink to="/dashboard">My Profile</NavLink>
+      <div className="bg-cyan-900 text-base-100 md:font-serif justify-center items-center md:justify-start w-full md:w-60 list-none flex md:flex-col p-6 md:h-screen shadow-2xl">
+        <ul className="grid gap-3 grid-flow-col md:grid-flow-row text-sm">
+          <li className="border border-gray-300 px-6 py-1 rounded-sm">
+            <NavLink to="/dashboard">
+              <span className="flex items-start justify-start">
+                <FaUserEdit className="md:mr-1 text-xl" />
+                My Profile
+              </span>
+            </NavLink>
           </li>
           {!isAdmin && (
             <>
-              <li>
-                <NavLink to="/dashboard/myOrders">My Orders</NavLink>
+              <li className="border border-gray-300 px-6 py-1 rounded-sm">
+                <NavLink to="/dashboard/myOrders">
+                  <span className="flex items-start justify-start">
+                    <FaHandHoldingUsd className="md:mr-1 text-xl" />
+                    My Orders
+                  </span>
+                </NavLink>
               </li>
-              <li>
-                <NavLink to="/dashboard/addReview">Add Review</NavLink>
+              <li className="border border-gray-300 px-6 py-1 rounded-sm">
+                <NavLink to="/dashboard/addReview">
+                  <span className="flex items-start justify-start">
+                    <FaRegComments className="md:mr-1 text-xl" />
+                    Add Review
+                  </span>
+                </NavLink>
               </li>
             </>
           )}
